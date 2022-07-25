d0bc75aa7910df3693d2f23ef333cc43ea460320
Andrej-Novik <andrejnovikweb@gmail.com>
2022-07-25 13:00:48 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/js/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js"
+  },
+  mode: "development",
+  devServer: {
+    port: 3500
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css"
+    }),
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+      },
+      { 
+        test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, 
+        type: "asset/resource" 
+      },
+    ]
+  }
+}
